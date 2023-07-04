aecd9cd047a16741fdbb0576b8c0cc7af85ecb6d
Renan Santos <renanwillian87@gmail.com>
2023-07-04 12:42:55 -0300

    chore: css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import { Post } from "./Post"
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import './styles.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post
         author="name 1"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
       />
       <Post
         author="name 2"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
-      />
-      <Post
-        author="name 3"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
-      />
-      <Post
-        author="name 4"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
       />
     </div>
