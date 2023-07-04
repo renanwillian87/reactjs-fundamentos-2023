0a40c842d7b2840c25cc44cdde978fc1269cc1ad
Renan Santos <renanwillian87@gmail.com>
2023-07-04 12:26:41 -0300

    chore: properties

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { Post } from "./Post"
 
 export function App() {
   return (
-    <>      
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-    </>
+    <div>
+      <Post
+        author="name 1"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
+      />
+      <Post
+        author="name 2"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
+      />
+      <Post
+        author="name 3"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
+      />
+      <Post
+        author="name 4"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
+      />
+    </div>
   )
 }
