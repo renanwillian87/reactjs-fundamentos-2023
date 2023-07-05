b2be4186a5cd39b9258761aa88dae3573c2d00a9
Renan Santos <renanwillian87@gmail.com>
2023-07-05 10:56:36 -0300

    chore: component post

---
+++ src/components/Post.jsx
@@ -0,0 +1,30 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/renanwillian87.png"/>
+                    <div className={styles.authorInfo}>
+                        <strong>Renan Santos</strong>
+                        <span>Fullstack Developer</span>
+                    </div>
+                </div>
+
+                <time title="04 de Julho às 21:58" dateTime="2023-07-04 21:58">Publicado há 1h</time>            
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href="">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="">#novoprojeto</a>{' '}
+                    <a href="">#nlw</a>{' '}
+                    <a href="">#rocketseat</a>
+                </p>
+            </div>
+        </article>
+    );
+}
