53a3a367f80d6f8b45c17a934912d87bf1ff2800
Renan Santos <renanwillian87@gmail.com>
2023-07-04 21:45:53 -0300

    chore: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header/>
-      <Post
-        author="name 1"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
-      />
-      <Post
-        author="name 2"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="name 1"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
+          />
+          <Post
+            author="name 2"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo minima eaque eveniet repellat voluptates laudantium adipisci fugit cumque similique perspiciatis praesentium, enim voluptas voluptatem recusandae libero accusantium rem sunt distinctio!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
