8b8a6f42eb67a76e5253b390db4493ebba94c317
Renan Santos <renanwillian87@gmail.com>
2023-07-05 16:43:46 -0300

    chore: component comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { Trash, ThumbsUp } from 'phosphor-react';
+
+import styles from './Comment.module.css';
+
+export function Comment() {
+    return(
+        <div className={styles.comment}>
+            <img src="https://github.com/renanwillian87.png" alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Renan Santos</strong>
+                            <time title="04 de Julho às 21:58" dateTime="2023-07-04 21:58">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
