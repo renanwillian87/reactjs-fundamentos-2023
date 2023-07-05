19caf48eaac6fd1c00b2bc23f428d798c1b9ce7e
Renan Santos <renanwillian87@gmail.com>
2023-07-05 19:06:29 -0300

    chore: apply state post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,12 +6,19 @@ import ptBr from 'date-fns/locale/pt-BR'
 import { Avatar } from './Avatar';
 import { Comment } from "./Comment";
 import styles from './Post.module.css'
+import { useState } from 'react';
 
 // author: { avatarUrl: "", name: "", role:"" }
 // publishedAt: Date
 // content: string
 
+
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2
+    ])
+
     const publishedAtFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
         locale: ptBr,
     });
@@ -20,6 +27,12 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBr,
         addSuffix: true,
     }) 
+
+    function handleCreateNewComment() {
+        event.preventDefault();
+
+        setComments([...comments, comments.length + 1])
+    }
 
     return (
         <article className={styles.post}>
@@ -47,7 +60,7 @@ export function Post({ author, publishedAt, content }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea
@@ -60,9 +73,9 @@ export function Post({ author, publishedAt, content }) {
             </form>
 
             <div className={styles.commentList}>
-                <Comment/>
-                <Comment/>
-                <Comment/>
+                {comments.map(comment => {
+                    return <Comment/>
+                })}
             </div>
         </article>
     );
